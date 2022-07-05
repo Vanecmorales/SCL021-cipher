7f6ffaf030967cbf62cf7fb5d7adaf838b514894
Vanecmorales <vanessagm.91@gmail.com>
2022-07-05 08:53:01 -0400

    Modificación del js: objeto.metodo

--- src/cipher.js
+++ src/cipher.js
@@ -1,23 +1,9 @@
-const cipher = {
-  // ...
-  function encode(mensaje, desplazamiento){
-    let resultado1 = "";
-    let alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  
-    desplazamiento = (desplazamiento % 26 + 26) % 26;
-  
-    if (mensaje){
-      for (let i = 0; i<mensaje.length; i++){
-        if (alfabeto.indexOf(mensaje[i])!=-1){
-          let posición = ((alfabeto.indexOf(mensaje[i])+desplazamiento)%26);
-          resultado1 +=alfabeto[posición];
-        }
-        else
-          resultado1 += mensaje[i];
-      }
-    }
-    return resultado1;
-  }
+const cipher ={
+  alfabeto: ["ABCDEFGHIJKLMNOPQRSTUVW"],
+  mensaje: document.getElementById("mensaje").value,
+  desplazamiento: document.getElementById ("offset").value,
+  resultado: document.getElementById("resultado").value,
+  encode()
 };
 
 export default cipher;
