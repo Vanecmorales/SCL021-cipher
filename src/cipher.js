ddd12df8e78e5e252e7744f09b30670df2b8f152
Vanecmorales <vanessagm.91@gmail.com>
2022-07-05 17:40:52 -0400

    desarrollo de fórmula encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,33 @@
-const cipher ={
-  alfabeto: ["ABCDEFGHIJKLMNOPQRSTUVW"],
-  mensaje: document.getElementById("mensaje").value,
-  desplazamiento: document.getElementById ("offset").value,
-  resultado: document.getElementById("resultado").value,
-  encode()
-};
+const cipher = {
+  
+  encode: function (inputMensaje, inputOffset) {
+    if (inputOffset === 'null' || inputMensaje.length === 0 || inputOffset === 0) {
+      throw new TypeError();
+    }
+    let codeChar = 0;
+    let mensajeEncoded = '';
+    for (let index = 0; index < inputMensaje.length; index++) {
 
+      let char = parseInt(inputMensaje.charCodeAt(index));
+
+      if (char >= 65 && char <= 90) {
+
+        codeChar = parseInt(((char - 65 + inputOffset) % 26) + 65);
+
+        mensajeEncoded += String.fromCharCode(codeChar);
+
+      } else if (char >= 97 && char <= 122) {
+        codeChar = parseInt(((char - 97 + inputOffset) % 26) + 97);
+
+        mensajeEncoded += String.fromCharCode(codeChar);
+      } else {
+
+        mensajeEncoded += String.fromCharCode(char);
+
+      }
+    }
+
+    return mensajeEncoded;
+  }
+};
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import cipher from './cipher.js';
+let inputOffset = parseInt(document.getElementById("offset").value);
+let inputMensaje = document.getElementById("mensaje").value;
+let mensajeEncoded = cipher.encode(inputOffset, inputMensaje);
+
+
+
+
+
+
+//evento click
 
     
 console.log(cipher);
+
+
